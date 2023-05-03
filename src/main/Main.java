88b888740297ffa01a0b87e85e70e4855f53264f
Logan Brown <2641407B@student.gla.ac.uk>
2023-05-03 14:48:45 +0100

    added GUI userlayer

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,11 +3,12 @@ package main;
 import board.Board;
 import board.BoardFactory;
 import userlayers.CommandLineUserLayer;
+import userlayers.GUIUserLayer;
 
 public class Main {
     public static void main(String[] args) {
 
-        Board board = BoardFactory.standardBoard(new CommandLineUserLayer());
+        Board board = BoardFactory.standardBoard(new GUIUserLayer());
         board.updateUserLayer();
 
         board.play();
--- src/userlayers/GUIUserLayer.java
+++ src/userlayers/GUIUserLayer.java
@@ -1,4 +1,79 @@
 package userlayers;
 
-public class GUIUserLayer {
+import board.Board;
+import main.Coordinate;
+import pieces.Piece;
+import players.Player;
+
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class GUIUserLayer implements UserLayer {
+
+    private static class Canvas extends java.awt.Canvas {
+        public void paint(Graphics g) {}
+
+        public Canvas(int size) {
+            this.setBackground(Color.BLACK);
+            this.setSize(size, size);
+        }
+    }
+
+    private Canvas canvas;
+    private Board board;
+
+    @Override
+    public Piece getPiece(Player p) {
+        return null;
+    }
+
+    @Override
+    public Coordinate getMove() {
+        return null;
+    }
+
+    @Override
+    public void update() {
+        Graphics g = canvas.getGraphics();
+        int max = Math.max(board.maxX, board.maxY);
+        int divSize = canvas.getWidth() / max;
+        int lineThickness = divSize / 30;
+
+        g.setColor(Color.white);
+        for (int i = 1; i < max; i++)
+            g.fillRect(i * divSize, 0, lineThickness, canvas.getHeight());
+        for (int i = 1; i < max; i++)
+            g.fillRect(0, i * divSize, canvas.getWidth(), lineThickness);
+
+
+    }
+
+    @Override
+    public void setBoard(Board board) {
+        this.board = board;
+    }
+
+    @Override
+    public String dialogue(String message) {
+        return null;
+    }
+
+    public GUIUserLayer() {
+        Frame f = new Frame("Chess");
+        int size = (int) (Math.min(Toolkit.getDefaultToolkit().getScreenSize().width, Toolkit.getDefaultToolkit().getScreenSize().height) * 0.95);
+        canvas = new Canvas((int) (size * 0.9));
+        canvas.setLocation((int) (size * 0.05), (int) (size * 0.05));
+        f.add(canvas);
+        f.setLayout(null);
+        f.setSize(size, size);
+        f.setVisible(true);
+
+        f.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+    }
 }
