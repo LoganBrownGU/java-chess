956403b9d3f6a9f57a525aa473ef6d9d62718635
Logan Brown <2641407B@student.gla.ac.uk>
2023-05-09 16:16:02 +0100

    added more piece models

--- src/userlayers/DisplayUpdater.java
+++ src/userlayers/DisplayUpdater.java
@@ -5,6 +5,7 @@ import entities.Camera;
 import entities.Entity;
 import entities.Light;
 import models.TexturedModel;
+import org.lwjgl.Sys;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.util.vector.Vector3f;
 import pieces.Piece;
@@ -15,6 +16,7 @@ import renderEngine.OBJLoader;
 import textures.ModelTexture;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class DisplayUpdater implements Runnable {
 
@@ -22,32 +24,41 @@ public class DisplayUpdater implements Runnable {
     private final Board board;
     private final Camera camera;
     private final Light light;
-    private final ArrayList<Entity> entities = new ArrayList<>();
+    private final HashMap<Piece, Entity> entities = new HashMap<>();
     private final Loader loader;
     private final G3DUserLayer parent;
 
     private void addPieces() {
-        entities.clear();
+        //entities.clear();
         for (Piece piece : board.getPieces()) {
-            TexturedModel model = new TexturedModel(OBJLoader.loadObjModel("assets/default_models/pawn.obj", loader),
-                    new ModelTexture(loader.loadTexture("assets/default_textures/" + piece.getPlayer().representation + ".png")));
+            if (entities.get(piece) != null) continue;
 
-            Entity entity = new Entity(model, new Vector3f(piece.getPosition().x * spacing, 0, piece.getPosition().y * spacing), 0, 0, 0, 1);
-            entities.add(entity);
+            try {
+                TexturedModel model = new TexturedModel(OBJLoader.loadObjModel("assets/default_models/Pawn.obj", loader),
+                        new ModelTexture(loader.loadTexture("assets/default_textures/" + piece.getPlayer().representation + ".png")));
+
+                Entity entity = new Entity(model, new Vector3f(piece.getPosition().x * spacing, 0, piece.getPosition().y * spacing), 0, 0, 0, 1);
+                entities.put(piece, entity);
+            } catch (NullPointerException ignored) {}
         }
     }
 
     @Override
     public void run() {
-        DisplayManager.createDisplay();
+        DisplayManager.createDisplay("Chess");
         MasterRenderer renderer = new MasterRenderer("assets/shaders", camera);
+        camera.setPosition(new Vector3f(((float) board.maxX / 2) * spacing, 10, (float) board.maxY * spacing * 2.5f));
 
         while (Display.isCreated()) {
+            camera.move();
             addPieces();
-            camera.setPosition(new Vector3f(((float) board.maxX / 2) * spacing, 40, ((float) board.maxY / 2) * spacing));
 
-            for (Entity entity : entities)
-                renderer.processEntity(entity);
+            for (Piece piece : board.getPieces()) {
+                Entity entity = entities.get(piece);
+                if (entity == null) continue;
+
+                renderer.processEntity(entities.get(piece));
+            }
 
             try {
                 renderer.render(light, camera);
@@ -67,7 +78,7 @@ public class DisplayUpdater implements Runnable {
 
         loader = new Loader();
         light = new Light(new Vector3f(20000, 20000, 2000), new Vector3f(1, 1, 1));
-        camera = new Camera(new Vector3f(0, 0, 0), new Vector3f(90, 0, 0), 45);
+        camera = new Camera(new Vector3f(0, 0, 0), new Vector3f(20, 0, 0), 45);
         this.parent = parent;
     }
 }
